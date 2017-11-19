7f682bd2801ce5d8e51b2c6cfdc63e3d8811a0d4
Paulo Baena <pb@datawerks.com>
2017-11-19 20:57:18 +0100

    add: finish chapter 2, add ex 3.1, 3.2

--- src/chapter2/Fibonacci.scala
+++ src/chapter2/Fibonacci.scala
@@ -2,7 +2,7 @@ package chapter2
 
 import scala.annotation.tailrec
 
-object Fibonacci extends App{
+object Fibonacci extends App {
   def fib(n: Int): Int = {
     @tailrec
     def fibRec(i: Int, i1: Int, pos: Int): Int =
---
+++ src/chapter3/Tail.scala
@@ -0,0 +1,14 @@
+package chapter3
+
+object Tail extends App {
+  def tail[A](List: List[A]): List[A] = List match {
+    case Nil => throw new UnsupportedOperationException
+    case Cons(y, ys) => ys
+  }
+
+  val l = List(1, 2, 3)
+  val t = tail(l)
+  assert(tail(l) == Cons(2,Cons(3,Nil)))
+  assert(tail(t) == Cons(3,Nil))
+
+}
